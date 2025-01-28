760900b8462def00d411f095c34e9abd9cbaef9e
RiericksonT <riericksontorres@gmail.com>
2025-01-28 14:45:13 -0300

    getting card details in integration service

--- src/integration/integration.module.ts
+++ src/integration/integration.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { IntegrationService } from './integration.service';
 import { IntegrationController } from './integration.controller';
+import { BmcService } from 'src/bmc/bmc.service';
+import { TrelloService } from 'src/trello/trello.service';
 
 @Module({
   controllers: [IntegrationController],
-  providers: [IntegrationService],
+  providers: [IntegrationService, BmcService, TrelloService],
 })
 export class IntegrationModule {}
--- src/integration/integration.service.ts
+++ src/integration/integration.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { BmcService } from 'src/bmc/bmc.service';
+import { TrelloService } from 'src/trello/trello.service';
 
 @Injectable()
 export class IntegrationService {
+  constructor(
+    private readonly bmcService: BmcService,
+    private readonly trelloService: TrelloService,
+  ) {}
   sync(body: any) {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-    return body;
+    const cardDetails = this.trelloService.getTrelloCard(
+      body.action.data.card.id,
+    );
+
+    console.log('cardDetails: ', cardDetails);
   }
 }
