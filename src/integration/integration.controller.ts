6d280e14b6c8f211d86d989f4d3c637d97eac425
RiericksonT <riericksontorres@gmail.com>
2025-01-28 13:17:31 -0300

    test weebhook

--- src/integration/integration.controller.ts
+++ src/integration/integration.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { IntegrationService } from './integration.service';
 
 @Controller('integration')
 export class IntegrationController {
   constructor(private readonly integrationService: IntegrationService) {}
+
+  @Post()
+  sync(@Body() body: any) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return this.integrationService.sync(body);
+  }
 }
--- src/integration/integration.service.ts
+++ src/integration/integration.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class IntegrationService {}
+export class IntegrationService {
+  sync(body: any) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return body;
+  }
+}
