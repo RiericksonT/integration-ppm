0e4841b00968dcab4c012e97aa33393208ecce34
RiericksonT <riericksontorres@gmail.com>
2025-02-04 14:47:19 -0300

    Feat: Add logic to find name

--- src/bmc/bmc.module.ts
+++ src/bmc/bmc.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { BmcService } from './bmc.service';
 import { BmcController } from './bmc.controller';
 import { TrelloModule } from 'src/trello/trello.module'; // Certifique-se de importar o módulo, não o serviço
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
-  imports: [TrelloModule], // Importa o módulo que fornece TrelloService
+  imports: [TrelloModule, CacheModule.register()], // Importa o módulo que fornece TrelloService
   controllers: [BmcController],
   providers: [BmcService],
   exports: [BmcService],
--- src/integration/integration.module.ts
+++ src/integration/integration.module.ts
@@ -3,8 +3,10 @@ import { IntegrationService } from './integration.service';
 import { IntegrationController } from './integration.controller';
 import { BmcService } from 'src/bmc/bmc.service';
 import { TrelloService } from 'src/trello/trello.service';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
+  imports: [CacheModule.register()],
   controllers: [IntegrationController],
   providers: [IntegrationService, BmcService, TrelloService],
 })
