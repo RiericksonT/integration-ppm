465c0f2e3ba61ce2357654db039726cb53a977dd
RiericksonT <riericksontorres@gmail.com>
2025-02-05 10:32:51 -0300

    Feat: finishing update logic

--- src/bmc/bmc.controller.ts
+++ src/bmc/bmc.controller.ts
@@ -1,16 +1,7 @@
-import { Body, Controller, Patch, Post } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { BmcService } from './bmc.service';
 
 @Controller('bmc')
 export class BmcController {
   constructor(private readonly bmcService: BmcService) {}
-  @Post('login')
-  async login(@Body() body: { username: string; password: string }) {
-    return await this.bmcService.login(body);
-  }
-
-  @Patch('update')
-  async update(@Body() body: any) {
-    return await this.bmcService.updateIncident(body);
-  }
 }
